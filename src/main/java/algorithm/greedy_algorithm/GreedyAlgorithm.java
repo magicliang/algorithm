b67886189a4ac45ade57655bd7f9958efc8aeab0
magicliang <magicliang@qq.com>
2025-08-30 11:03:59 +0800

    初始化贪心算法

---
+++ src/main/java/algorithm/greedy_algorithm/GreedyAlgorithm.java
@@ -0,0 +1,7 @@
+package algorithm.greedy_algorithm;
+
+public class GreedyAlgorithm {
+    public static void main(String[] args) {
+        System.out.println("This is a placeholder for Greedy Algorithm implementations.");
+    }
+}
