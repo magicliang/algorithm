24d5cd451ae575baef302a6da6eab83e600e7cc1
magicliang <magicliang@qq.com>
2025-08-30 23:30:15 +0800

    引入第一个暴力法

--- src/main/java/algorithm/foundations/divideconquer/MaxSubArray.java
+++ src/main/java/algorithm/foundations/divideconquer/MaxSubArray.java
@@ -23,7 +23,6 @@ package algorithm.foundations.divideconquer;
 public class MaxSubArray {
 
     public int maxSubArrayBrutalForce1(int[] arr) {
-
         int max = arr[0];
         for (int i = 0; i < arr.length; i++) {
             for (int j = i + 1; j < arr.length; j++) {
